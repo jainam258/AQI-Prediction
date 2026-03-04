3a8883a5d45a415dc4470889ebfd297d022a7935
jainam258 <jainamshah2589@gmail.com>
2026-03-04 19:42:45 +0530

    Refactor model file IDs to use shortened format for downloads

--- download_models.py
+++ download_models.py
@@ -5,12 +5,12 @@ import os
 os.makedirs("artifacts", exist_ok=True)
 
 files = {
-    "artifacts/aqi_model.joblib":          "https://drive.google.com/file/d/1yyufwqdMzipgNN9cN3gRA_EOSqYT-zwx/view?usp=sharing",
-    "artifacts/city_aqi_model.joblib":     "https://drive.google.com/file/d/1KROU3B_vKwc1NCnohWeWWgEpSdb6SeGp/view?usp=sharing",
-    "artifacts/city_label_encoder.joblib": "https://drive.google.com/file/d/1V-dpmuxZlZkLBRmiIHIBktW-Rq6T48Co/view?usp=sharing",
-    "artifacts/features.joblib":           "https://drive.google.com/file/d/11KXASi35lBo_fXXgVAi-P2A-t7_587pN/view?usp=sharing",
-    "artifacts/imputer.joblib":            "https://drive.google.com/file/d/1iU95VtGgPA4KLxrwnMF2vdKrCeGhvaq_/view?usp=sharing",
-    "artifacts/scaler.joblib":             "https://drive.google.com/file/d/1v_9FfD0jlZQszWbKylm15G-8QmWVojqZ/view?usp=sharing",
+    "artifacts/aqi_model.joblib":          "1yyufwqdMzipgNN9cN3gRA_EOSqYT-zwx",
+    "artifacts/city_aqi_model.joblib":     "1KROU3B_vKwc1NCnohWeWWgEpSdb6SeGp",
+    "artifacts/city_label_encoder.joblib": "1V-dpmuxZlZkLBRmiIHIBktW-Rq6T48Co",
+    "artifacts/features.joblib":           "11KXASi35lBo_fXXgVAi-P2A-t7_587pN",
+    "artifacts/imputer.joblib":            "1iU95VtGgPA4KLxrwnMF2vdKrCeGhvaq_",
+    "artifacts/scaler.joblib":             "1v_9FfD0jlZQszWbKylm15G-8QmWVojqZ",
 }
 
 for path, file_id in files.items():
